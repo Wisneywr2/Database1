96a406e5e7457b51c7cec21813a46fbccde44636
wisne <wisne@LAPTOP-M62O98BH>
2022-03-20 14:52:05 -0300

    Adicionar arquivos de projeto.

---
+++ Database1/dbo/Tables/filmes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[filmes] (
+    [cod_filmes]                INT          NOT NULL,
+    [nome_filmes]               VARCHAR (30) NOT NULL,
+    [genero_filmes]             VARCHAR (30) NOT NULL,
+    [data_de_lancamento_filmes] DATE         NOT NULL,
+    [pais_de_origem_filmes]     VARCHAR (30) NOT NULL,
+    [tipo_de_lancamento]        VARCHAR (30) NOT NULL,
+    PRIMARY KEY CLUSTERED ([cod_filmes] ASC)
+);
+
---
+++ Database1/dbo/Tables/locadora.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[locadora] (
+    [cod_locadora]    INT          NOT NULL,
+    [nome_locadora]   VARCHAR (30) NOT NULL,
+    [bairo_locadora]  VARCHAR (40) NOT NULL,
+    [rua_locadora]    VARCHAR (40) NOT NULL,
+    [cidade_locadora] VARCHAR (40) NOT NULL,
+    [cep_locadora]    VARCHAR (8)  NOT NULL,
+    [cnpj_locadora]   VARCHAR (14) NOT NULL,
+    PRIMARY KEY CLUSTERED ([cod_locadora] ASC),
+    UNIQUE NONCLUSTERED ([cnpj_locadora] ASC)
+);
+
